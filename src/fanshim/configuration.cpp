9136ae95c4651f500f8f2662c6b3af0c720a252e
Joe Porembski <jporembski@gmail.com>
2022-09-28 15:19:00 -0400

    Account for optional fields
    This commit updates the implementation of the driver configuration
    to permit any field in the configuration file to be optional, save
    for one: `on-threshold` and `off-threshold` _must_ be specified
    as a pair.
    
    This will make it easier for users to only override behavior
    they wish to change.

--- src/fanshim/configuration.cpp
+++ src/fanshim/configuration.cpp
@@ -1,5 +1,7 @@
 #include "fanshim/configuration.hpp"
 
+#include "fanshim/logger.hpp"
+
 #include <nlohmann/json.hpp>
 
 #include <cerrno>
@@ -22,12 +24,61 @@ inline constexpr std::string_view OUTPUT_FILE = "output-file";
 
 static bool isValid(const json& configuration)
 {
-    // Optional fields?
-    return !configuration.empty() && configuration[ON_THRESHOLD].is_number() && configuration[OFF_THRESHOLD].is_number() && configuration[DELAY].is_number() &&
-           configuration[BRIGHTNESS].is_number() && configuration[BLINK].is_number() && configuration[BREATH_BRIGHTNESS].is_number() &&
-           configuration[ON_THRESHOLD].get<int32_t>() >= configuration[OFF_THRESHOLD].get<int32_t>() && configuration[DELAY].get<uint32_t>() > 0 &&
-           configuration[BRIGHTNESS].get<int32_t>() > 0 && configuration[BLINK].get<int32_t>() > 0 && configuration[BLINK].get<int32_t>() < 3 &&
-           configuration[BREATH_BRIGHTNESS].get<int32_t>() > 0 && configuration[BREATH_BRIGHTNESS].get<int32_t>() <= 31;
+    // Rules for a "valid" configuration:
+    //      1. It must not be empty
+    //      2. If it contains On Threshold, it must contain Off Threshold too.
+    //          a. On Threshold and Off Threshold must be unsigned integers.
+    //          b. On Threshold must be greater than Off Threshold.
+    //      3. If it contains Delay, Delay must be an unsigned integer.
+    //      4. If it contains Brightness, Brightness must be an unsigned integer.
+    //      5. If it contains Blink, Blink must be an unsigned integer.
+    //          a. Blink must be a valid BlinkType.
+    //      6. If it contains Breath Brightness, Breath Brightness must be an unsigned integer.
+    //          a. Breath Brightness must be less than 31
+    //      7. If it contains Output File, Output File must be a string.
+
+    if (configuration.empty()) {
+        return false;
+    }
+
+    if ((configuration.contains(ON_THRESHOLD) && !configuration.contains(OFF_THRESHOLD)) || (!configuration.contains(ON_THRESHOLD) && configuration.contains(OFF_THRESHOLD))) {
+        return false;
+    }
+
+    if (configuration.contains(ON_THRESHOLD) && configuration.contains(OFF_THRESHOLD)) {
+        if (!configuration[ON_THRESHOLD].is_number() || !configuration[OFF_THRESHOLD].is_number() ||
+            configuration[ON_THRESHOLD].get<uint8_t>() < configuration[OFF_THRESHOLD].get<uint8_t>()) {
+            return false;
+        }
+    }
+
+    if (configuration.contains(DELAY) && !configuration[DELAY].is_number()) {
+        return false;
+    }
+
+    if (configuration.contains(BRIGHTNESS) && !configuration[BRIGHTNESS].is_number()) {
+        return false;
+    }
+
+    if (configuration.contains(BLINK)) {
+        if (!configuration[BLINK].is_number() || configuration[BLINK].get<uint8_t>() > static_cast<uint8_t>(BlinkType::BREATHE)) {
+            return false;
+        }
+    }
+
+    if (configuration.contains(BREATH_BRIGHTNESS)) {
+        if (!configuration[BREATH_BRIGHTNESS].is_number() || configuration[BLINK].get<uint8_t>() > 31) {
+            return false;
+        }
+    }
+
+    if (configuration.contains(OUTPUT_FILE)) {
+        if (!configuration[OUTPUT_FILE].is_string()) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 Configuration::Configuration() : Configuration(std::filesystem::path(DEFAULT_CONFIGURATION_FILE))
@@ -93,17 +144,32 @@ void Configuration::_load(const std::filesystem::path& configuration_file)
     config_stream.close();
 
     if (!isValid(config)) {
+        logger().error("Configuration file {} is not valid.", configuration_file.native());
         return;
     }
 
-    _on_threshold = config[ON_THRESHOLD].get<int32_t>();
-    _off_threshold = config[OFF_THRESHOLD].get<int32_t>();
-    _delay = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(config[DELAY].get<uint32_t>()));
-    _brightness = config[BRIGHTNESS].get<uint8_t>();
-    _blink = static_cast<BlinkType>(config[BLINK].get<uint8_t>());
-    _breath_brightness = config[BREATH_BRIGHTNESS].get<uint8_t>();
+    if (config.contains(ON_THRESHOLD)) {
+        _on_threshold = config[ON_THRESHOLD].get<uint8_t>();
+        _off_threshold = config[OFF_THRESHOLD].get<uint8_t>();
+    }
 
-    if (config[OUTPUT_FILE].is_string()) {
+    if (config.contains(DELAY)) {
+        _delay = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(config[DELAY].get<uint8_t>()));
+    }
+
+    if (config.contains(BRIGHTNESS)) {
+        _brightness = config[BRIGHTNESS].get<uint8_t>();
+    }
+
+    if (config.contains(BLINK)) {
+        _blink = static_cast<BlinkType>(config[BLINK].get<uint8_t>());
+    }
+
+    if (config.contains(BREATH_BRIGHTNESS)) {
+        _breath_brightness = config[BREATH_BRIGHTNESS].get<uint8_t>();
+    }
+
+    if (config.contains(OUTPUT_FILE)) {
         _output_file = std::filesystem::path(config[OUTPUT_FILE].get<std::string>());
     }
 }
